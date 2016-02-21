1e64e728cd7d4e87126849f5946c6738fcc254d8
Artem Pakk <apakk@me.com>
2016-02-21 17:15:41 +0200

    Show overdue items on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @plugins = current_user.plugins
+    @overdue_items = current_user.payable_items.overdue.includes(:payable)
   end
 end
