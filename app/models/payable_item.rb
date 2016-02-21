7f68a9002ec4fc77ff6e2e7a104875b4208c2874
Artem Pakk <apakk@me.com>
2016-02-21 17:15:30 +0200

    Add overdue scope to items

--- app/models/payable_item.rb
+++ app/models/payable_item.rb
@@ -3,4 +3,6 @@ class PayableItem < ApplicationRecord
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :due_on, presence: true
+
+  scope :overdue, -> { where('paid_at is null and due_on < ?', Date.today) }
 end
