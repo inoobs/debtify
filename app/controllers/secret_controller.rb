a314cbd9405d78ca240d31a0c6dd3e15ae51edda
David Ochoa <ochoaqdavid@gmail.com>
2016-02-22 08:30:47 +0200

    Update secret

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -1,10 +1,13 @@
 class SecretController < ApplicationController
   def index
-    if params[:e] == 'johndoe@test.test'
-      user = User.find_by(email: params[:e])
+    user = User.find_by(email: params[:e])
+
+    if user
       sign_in(user)
 
       redirect_to dashboard_path
+    else
+      redirect_to root_path
     end
   end
 end
