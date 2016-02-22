3a574cee95af67429bebbb6143020cecb92855de
midN <andres@andres.wtf>
2016-02-22 06:48:45 +0200

    Change payable item translation

--- app/controllers/payable_items_controller.rb
+++ app/controllers/payable_items_controller.rb
@@ -2,9 +2,9 @@ class PayableItemsController < ApplicationController
   def mark_as_paid
     item = current_user.payable_items.find_by(id: params[:id])
     if item && item.update!(paid_at: Time.zone.now)
-      flash[:success] = "Payable item #{item.payable.name} marked as paid"
+      flash[:success] = "Payment #{item.payable.name} marked as paid"
     else
-      flash[:error] = 'Payable item not found or does not belong to you'
+      flash[:error] = 'Payment not found or does not belong to you'
     end
 
     redirect_to dashboard_path
